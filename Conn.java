f84647b72ab3f75b20a66c9065a0ee76fdddbdeb
Aryandh01 <140616024+Aryandh01@users.noreply.github.com>
2023-07-26 18:29:31 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bank.management.system;
+
+import java.sql.*;
+
+/**
+ *
+ * @author dell
+ */
+public class Conn {
+    Connection c;
+    Statement s;
+    
+    public Conn()  {
+        try {
+            
+            
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "Aryan8284820080");
+            s = c.createStatement();
+            
+        }catch (Exception e)  {
+            System.out.println(e);
+        }
+    }
+    
+}
